83e980831919a001efccfec9c5f7a4b990346ea7
Eugene Genov <genov.work@gmail.com>
2017-03-28 09:19:12 +0300

    Добавлен процесс пересчета плановых ресурсов

--- SkProject/Schemas/SkSchema1Detail/SkSchema1Detail.js
+++
@@ -1,8 +0,0 @@
-define("SkSchema1Detail", [], function() {
-	return {
-		entitySchemaName: "SkProjectJiraTask",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
